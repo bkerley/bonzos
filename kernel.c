cee9c084d1301c4c7a32ebd9f875560c3c8c2357
Bryce Kerley <bkerley@brycekerley.net>
2011-07-28 09:17:45 -0600

    runs

--- kernel.c
+++ kernel.c
@@ -11,11 +11,20 @@ void kmain( void* mbd, unsigned int magic )
    /* (http://www.gnu.org/software/grub/manual/multiboot/multiboot.html#multiboot_002eh) */
    /* or do your offsets yourself. The following is merely an example. */ 
    char * boot_loader_name =(char*) ((long*)mbd)[16];
+   char* loader_name_cursor = boot_loader_name;
  
    /* Print a letter to screen to see everything is working: */
    unsigned char *videoram = (unsigned char *) 0xb8000;
-   videoram[0] = 65; /* character 'A' */
-   videoram[1] = 0x07; /* forground, background color. */
+   unsigned char *videoram_cursor = videoram;
+
+   while (*loader_name_cursor != 0) {
+     *videoram_cursor = *loader_name_cursor;
+     videoram_cursor++;
+     *videoram_cursor = 0x07;
+     videoram_cursor++;
+     loader_name_cursor++;
+   }
  
    /* Write your kernel here. */
+
 }
